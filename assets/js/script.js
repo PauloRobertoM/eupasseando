10cce67da48c9df2996f36407f13c6ef637d73b7
Paulo Alves <front@agencianovi.com.br>
2018-05-18 16:31:05 -0300

    Avançando

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,4 +17,22 @@
             }
         }
     });
+    $('#owl-comentarios').owlCarousel({
+        loop: false,
+        autoplay: false,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
 })(jQuery, window);
