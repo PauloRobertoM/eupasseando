28279c2fd5bd7ec33994d58a281af4380e926e49
Paulo Alves <front@agencianovi.com.br>
2018-05-24 16:53:05 -0300

    Avançando

---
+++ cadastro.php
@@ -0,0 +1,110 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="interna login cadastro">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#">Entrar no leilão</a></li>
+			</ol>
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<h2 class="titulo-geral">Faça o seu cadastro</h2>
+				</div><!-- md-6 -->
+			</div><!-- row -->
+
+			<form>
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<h4><img src="assets/images/ico-dados.png" alt=""> 1. Dados pessoais</h4>
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Nome</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label>Último sobrenome</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Data de nascimento</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label>Sexo</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Foto de perfil</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label>Anexe um arquivo de imagem com até 500kb.</label>
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<div class="borda"></div>
+					</div><!-- md-6 -->
+				</div><!-- row -->
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<h4><img src="assets/images/ico-cartao.png" alt=""> 2. Dados de pagamento</h4>
+						<h5>CARTÃO DE CRÉDITO</h5>
+						<label>Nome impresso no cartão</label>
+						<input type="text" class="form-control"/>
+						<label>Número do cartão</label>
+						<input type="text" class="form-control"/>
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Validade</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label>Código de segurança</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<div class="borda"></div>
+					</div><!-- md-6 -->
+				</div><!-- row -->
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<h4><img src="assets/images/ico-cartao.png" alt=""> 3. E-mail para contato</h4>
+						<label>E-mail</label>
+						<input type="text" class="form-control"/>
+						<label>Confime o e-mail</label>
+						<input type="text" class="form-control"/>
+						<label for="">Quero receber ofertas e informações do Eu passeando.</label>
+						<div class="borda"></div>
+					</div><!-- md-6 -->
+				</div><!-- row -->
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<h4><img src="assets/images/ico-cartao.png" alt=""> 4. Sua senha</h4>
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Login</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+						</div><!-- row -->
+						<div class="row">
+							<div class="col-md-6 col-sm-6">
+								<label>Senha (no mínimo 8 dígitos)</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<div class="col-md-6 col-sm-6">
+								<label>Confime sua senha</label>
+								<input type="text" class="form-control"/>
+							</div><!-- md-6 -->
+							<label for="">Declaro que li as condições de compra e políticas de cancelamento e estou de acordo com os termos das tarifas e serviços oferecidos pela empresa.</label>
+						</div><!-- row -->
+						<div class="borda"></div>
+					</div><!-- md-6 -->
+				</div><!-- row -->
+			</form>
+		</div><!-- container -->
+		<img src="assets/images/back-vaije.png" alt="" class="img-responsive">
+	</section><!-- interna -->
+
+<?php include 'components/footer.php'; ?>
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="interna login">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="#">Eu passeando</a></li>
+				<li><a href="#">Nossos pacotes</a></li>
+				<li><a href="#">Paris</a></li>
+				<li class="active">Entrar no leilão</li>
+			</ol>
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<h2 class="titulo-geral">Eu passeando</h2>
+					<p>Breve explicação de como funciona o leilão. Breve explicação de como funciona o leilão. Breve explicação de como funciona o leilão. Breve explicação de como funciona o leilão. <span>Boa sorte e divirta-se. =D</span></p>
+				</div><!-- md-6 -->
+			</div><!-- row -->
+			
+			<div class="row">
+				<div class="col-md-4 col-sm-4">
+					<form>
+						<input type="text" class="form-control" placeholder="Login" />
+						<input type="password" class="form-control" placeholder="Senha" />
+						<div class="botoes">
+							<a href="" class="btn">CADASTRE-SE</a>
+							<a href="" class="btn pull-right">ENTRAR</a>
+							<a href="" class="esqueceu">Esqueceu a senha?</a>
+						</div><!-- botoes -->
+					</form>
+				</div><!-- md-4 -->
+			</div><!-- row -->
+		</div><!-- container -->
+		<img src="assets/images/back-vaije.png" alt="" class="img-responsive">
+	</section><!-- interna -->
+
+<?php include 'components/footer.php'; ?>
