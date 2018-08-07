6f784dd4f895ef5110b3b31bba0b3f2a3f3bd798
Paulo Alves <front@agencianovi.com.br>
2018-08-07 11:34:01 -0300

    Tela de Espera

---
+++ tela-de-espera.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+	    <meta charset="UTF-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	    <title>EuPasseando</title>
+
+	    <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
+	    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
+
+	    <link rel='stylesheet' href='assets/css/bootstrap.min.css' type='text/css' />
+	    <link rel='stylesheet' href='assets/css/hover-min.css' type='text/css' />
+	    <link rel='stylesheet' href='assets/css/owl.carousel.min.css' type='text/css' />
+	    <link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+	    <link rel='stylesheet' href='assets/css/style.css' type='text/css' />
+	</head>
+
+	<body>
+		<section class="tela-de-espera">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<h1>Nosso lance é viajar.<br /><span>Qual é o seu?</span></h1>
+						<p>Conheça o Eu Passeando, a sua mais nova forma de conhecer o mundo. Com o conceito de trip sale, você economiza o seu dinheiro dando lances em <span>pacotes turísticos exclusivos</span>, podendo conseguir aquela viagem dos seus sonhos de maneira muito mais fácil e muito mais barata.</p>
+					</div><!-- md-6 -->
+					<div class="col-md-6 col-sm-6">
+						<div class="item">
+							<div class="conteudo">
+								<h4>Quer saber antes de todo mundo qual vai ser o nosso próximo destino?<br /><span>Deixa seu email com a gente.</span></h4>
+								<form action="">
+									<input type="text" class="form-control" placeholder="NOME:">
+									<input type="email" class="form-control" placeholder="EMAIL:">
+									<input type="text" class="form-control" placeholder="PRA ONDE VOCÊ QUER VIAJAR?">
+									<button>ENVIAR</button>
+								</form>
+								<div class="redes-sociais">
+									<a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+									<a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+								</div><!-- redes-sociais -->
+							</div><!-- conteudo -->
+						</div><!-- item -->
+					</div><!-- md-6 -->
+				</div><!-- row -->
+			</div><!-- container -->
+		</section><!-- tela-de-espera -->
+
+        <script type='text/javascript' src='assets/js/jquery.js'></script>
+        <script type='text/javascript' src='assets/js/bootstrap.min.js'></script>
+        <script src="https://use.fontawesome.com/6963733f46.js"></script>
+    </body>
+</html>
